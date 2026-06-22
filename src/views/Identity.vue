cef7876ad91e0c740d018ecd31eba3435fe8d44b
Melvin Loos <melvin@melvinloos.nl>
2026-06-22 05:41:18 +0200

    Refactor /identity route to combine SheetHeader, AbilityScores, PersonalityBlock, and SavingThrows

--- src/views/Identity.vue
+++ src/views/Identity.vue
@@ -3,20 +3,30 @@ import { useCharacterStore } from '@/stores/character'
 import SheetHeader from '@/components/sheet/SheetHeader.vue'
 import AbilityScores from '@/components/sheet/AbilityScores.vue'
 import PersonalityBlock from '@/components/sheet/PersonalityBlock.vue'
+import SavingThrows from '@/components/sheet/SavingThrows.vue'
 
 const store = useCharacterStore()
 </script>
 
 <template>
-  <div class="flex flex-col gap-8">
+  <div class="flex flex-col gap-8 w-full max-w-7xl mx-auto">
     <SheetHeader />
-    <div class="grid grid-cols-1 xl:grid-cols-2 gap-8 items-start">
-      <div class="flex flex-col gap-8">
+    
+    <div class="grid grid-cols-1 lg:grid-cols-12 gap-8 items-stretch">
+      <!-- Left Column: Personality -->
+      <section class="lg:col-span-5 flex flex-col gap-8">
+        <PersonalityBlock class="h-full" />
+      </section>
+      
+      <!-- Right Column: Ability Scores -->
+      <section class="lg:col-span-7 flex flex-col gap-8">
         <AbilityScores />
-      </div>
-      <div class="flex flex-col gap-8">
-        <PersonalityBlock />
-      </div>
+      </section>
+    </div>
+
+    <!-- Full Width: Saving Throws -->
+    <div class="w-full">
+      <SavingThrows />
     </div>
   </div>
 </template>
