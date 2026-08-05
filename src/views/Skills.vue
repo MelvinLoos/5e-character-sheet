bec274081c4736944e373f33bc4e1e70be9a2757
Melvin Loos <melvin@melvinloos.nl>
2026-08-05 16:48:38 +0200

    fix(ui): remove h-full from Skills and Narrative views to keep mobile tab bar in frame (closes #78)

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -4,7 +4,7 @@ const store = useCharacterStore()
 </script>
 
 <template>
-  <div class="flex flex-col h-full">
+  <div class="flex flex-col">
     <header class="mb-8 flex justify-between items-end border-b border-surface-variant pb-4">
       <div>
         <h2 class="font-display-lg text-display-lg text-tertiary">Skills & Proficiencies</h2>
@@ -26,7 +26,7 @@ const store = useCharacterStore()
       </div>
     </header>
 
-    <div class="flex flex-col gap-2 flex-1">
+    <div class="flex flex-col gap-2">
       <SavingThrows />
       <SkillsList />
     </div>
